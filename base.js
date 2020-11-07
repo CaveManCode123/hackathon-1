ac7fc1e9c6993c25d5a99144fb41f2f0ebe17d7c
CaveManCode123 <72443745+CaveManCode123@users.noreply.github.com>
2020-11-08 01:16:12 +0200

    Update base.js

--- base.js
+++ base.js
@@ -14,11 +14,10 @@ function jump() {
 let isAlive = setInterval(function () {
     
       let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
-    
+       //console.log(CactusLeft)
       let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left")
       );
-    
-  
+     //console.log(CactusLeft)
     if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
 
        alert("you ded");
